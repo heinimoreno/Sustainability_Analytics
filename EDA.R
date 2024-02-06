c3b5bf4062ba39d6d29797e5dddb221f1510ac4e
Milica Pajkic <milica.pajkic@stud.hslu.ch>
2024-02-06 13:17:41 +0100

    tre200nn daten schwyz
    daten neu laden tre200nn

--- EDA.R
+++ EDA.R
@@ -1,10 +1,17 @@
 library(tidyverse)
 
-# Read a CSV file into a data frame
+# Engelberg
 engelberg <- read.csv("Daten/Engelberg_data.csv",sep=';', na.strings=c('-',''))
 
 engelberg$time <- as.Date(as.character(engelberg$time), format = "%Y%m%d")
+engelberg_clean <- engelberg %>% select('stn',
+                                        'time',
+                                        'tre200nn')
 
-
-# View the first few rows
+# Schwyz
 schwyz <- read.csv("Daten/Schwyz_data.csv",sep=';', na.strings=c('-',''))
+schwyz$time <- as.Date(as.character(schwyz$time), format = "%Y%m%d")
+schwyz_clean <- schwyz %>% select('stn',
+                                 'time',
+                                 'tre200nn')
+
