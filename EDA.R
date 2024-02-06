0288ecd41ebe4c7027188978f6add4537d12e2d9
Milica Pajkic <milica.pajkic@stud.hslu.ch>
2024-02-06 15:05:02 +0100

    kleine EDA

--- EDA.R
+++ EDA.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-# Engelberg
+#### Engelberg ####
 engelberg <- read.csv("Daten/Engelberg_data.csv",sep=';', na.strings=c('-',''))
 
 engelberg$time <- as.Date(as.character(engelberg$time), format = "%Y%m%d")
@@ -8,26 +8,33 @@ engelberg_clean <- engelberg %>% select('stn',
                                         'time',
                                         'tre200nn')
 
-# Schwyz
-# schwyz <- read.csv("Daten/Schwyz_data.csv",sep=';', na.strings=c('-',''))
-# schwyz_clean <- schwyz %>% select('stn',
-#                                  'time',
-#                                  'tre200nn')
-schwyz.1 <- read.csv("Daten/Schwyz_data_1.csv",sep=';', na.strings=c('-',''))
-schwyz.1$time <- as.Date(as.character(schwyz.1$time), format = "%Y%m%d")
-schwyz.1_clean <- schwyz.1 %>% select('stn',
-                                  'time',
-                                  'tre200nn')
-
-
-schwyz.alle <- read.csv("Daten/schwyz_alle_jahre.csv",sep=';', na.strings=c('-',''))
-# Convert integer dates to Date objects
-schwyz.alle$time <- as.Date(as.character(schwyz.alle$time), format = "%Y%m%d")
-schwyz.alle_clean <- schwyz.alle %>% select('stn',
-                                      'time',
-                                      'tre200nn')
-
+# NA anschauen
 str(engelberg_clean)
+summary(engelberg_clean)
+
+
+#### Rigi ####
+seeboden <- read.csv("Daten/Seebodenalp_data.csv",sep=';', na.strings=c('-',''))
+# Convert integer dates to Date objects
+seeboden$time <- as.Date(as.character(seeboden$time), format = "%Y%m%d")
+seeboden_clean <- seeboden %>% select('stn',
+                                            'time',
+                                            'tre200nn')
+
+# NA anschauen
+str(seeboden_clean)
+summary(seeboden_clean)
+
+
+# EDA
+ggplot(engelberg_clean) + 
+  geom_histogram(mapping = aes(x = tre200nn), binwidth = 0.5)
+
+
+
+
+
+
 
 
 
