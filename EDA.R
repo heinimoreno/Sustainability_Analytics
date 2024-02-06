bc12857c098e473755ec1957e6cf0b9993e73fe5
Milica Pajkic <milica.pajkic@stud.hslu.ch>
2024-02-06 14:30:07 +0100

    Seebodenalp
    daten Seebodenalp

--- EDA.R
+++ EDA.R
@@ -9,9 +9,26 @@ engelberg_clean <- engelberg %>% select('stn',
                                         'tre200nn')
 
 # Schwyz
-schwyz <- read.csv("Daten/Schwyz_data.csv",sep=';', na.strings=c('-',''))
-schwyz$time <- as.Date(as.character(schwyz$time), format = "%Y%m%d")
-schwyz_clean <- schwyz %>% select('stn',
-                                 'time',
-                                 'tre200nn')
+# schwyz <- read.csv("Daten/Schwyz_data.csv",sep=';', na.strings=c('-',''))
+# schwyz_clean <- schwyz %>% select('stn',
+#                                  'time',
+#                                  'tre200nn')
+schwyz.1 <- read.csv("Daten/Schwyz_data_1.csv",sep=';', na.strings=c('-',''))
+schwyz.1$time <- as.Date(as.character(schwyz.1$time), format = "%Y%m%d")
+schwyz.1_clean <- schwyz.1 %>% select('stn',
+                                  'time',
+                                  'tre200nn')
+
+
+schwyz.alle <- read.csv("Daten/schwyz_alle_jahre.csv",sep=';', na.strings=c('-',''))
+# Convert integer dates to Date objects
+schwyz.alle$time <- as.Date(as.character(schwyz.alle$time), format = "%Y%m%d")
+schwyz.alle_clean <- schwyz.alle %>% select('stn',
+                                      'time',
+                                      'tre200nn')
+
+str(engelberg_clean)
+
+
+
 
