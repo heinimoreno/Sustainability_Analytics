589e8574862321cbd2fa928e0de5b78a7b337c28
Milica Pajkic <milica.pajkic@stud.hslu.ch>
2024-02-08 14:17:50 +0100

    Trend abline

--- timeSeriesTemp.R
+++ timeSeriesTemp.R
@@ -43,6 +43,7 @@ range(engelberg_clean$time)
 interval(start = head(engelberg_clean$date)[1], end = tail(engelberg_clean$date)[1])
 
 #### Time Series ####
+#### > Year ####
 engelberg_clean <- engelberg_clean %>%
   # Assuming 'time' is already a Date object. If not, convert it first with as.Date()
   filter(year(time) > 1989) %>%
@@ -52,7 +53,11 @@ ts_engelberg <- ts(data = engelberg_clean$temp,
                start = c(1990,01),
                frequency = 365)
 
-autoplot(ts_engelberg)
+engelberg_clean_trend <- lm(engelberg_clean$temp~engelberg_clean$time)
+plot(ts_engelberg)
+abline(engelberg_clean_trend, col = 'red')
+
+#autoplot(ts_engelberg)
 
 # Decompose
 ts_engelberg_dc <- decompose(ts_engelberg)
@@ -71,9 +76,15 @@ pacf(ts_engelberg_dc$random, na.action = na.pass)
 PAutoCorrelation <- pacf(ts_engelberg_dc$random, na.action = na.pass, plot=FALSE)
 plot(PAutoCorrelation, main = "Whole Year PACF")
 
-#### > Arima Model ####
-# engelberg_auto <- auto.arima(ts_engelberg, D=1, d=1, seasonal = T)
-# engelberg_auto
+#Arima
+# # Fitting to PACF 
+window <- list(start=1990,end=2023)
+
+temp_comp_random_y <- data.frame(Date=time(ts_engelberg_dc$random), Random=ts_engelberg_dc$random)
+temp_comp_random_y %<>% filter(Date>=window$start&Date<window$end)
+temp_comp_random_y_ts <- ts(temp_comp_random_y$Random)
+arima(temp_comp_random_y_ts, c(1,0,0))
+
 
 #### Data only winter ####
 
@@ -157,14 +168,42 @@ temp_comp_random_ts <- ts(temp_comp_random$Random)
 arima(temp_comp_random_ts, c(1,0,0))
 
 
-# daily_auto <- auto.arima(ts_daily, D=1, d=1)
-# daily_auto.1 <- auto.arima(ts_daily)
-# daily_auto
-# daily_auto.1
-
-
 # To do
 #### Soll PACF vs. PACF Winter
-#### Nur einen Winter rausnehmen und dann Arima
-#### Nachfolgende Tage unter 0
+#### Nur einen Winter rausnehmen und dann Arima ####
+one_engelberg <- engelberg_filtered %>% 
+  filter(year(time) > 1999 &
+           year(time) < 2003)
+
+glimpse(one_engelberg)
+
+ts_one <- ts(data = one_engelberg$temp, frequency = 122)
+autoplot(ts_one)
+
+# Decompose
+ts_one_dc <- decompose(ts_one)
+plot(ts_one_dc)
+
+# Stationary?
+adf.test(ts_one)
+## it is stationary
+
+acf(ts_one)
+acf(ts_one_dc$random,na.action=na.pass)
+# Plotting
+PAutoCorrelation_m <- pacf(ts_one_dc$random,na.action=na.pass, plot=FALSE)
+plot(PAutoCorrelation_m, main = "Winter month PACF 2000")
+
+# Arima Model
+# # Fitting to PACF 
+window <- list(start=2000,end=2002)
+
+temp_comp_random_one <- data.frame(Date=time(ts_one_dc$random), Random=ts_one_dc$random)
+temp_comp_random_one %<>% filter(Date>=window$start&Date<window$end)
+temp_comp_random_one_ts <- ts(temp_comp_random_one$Random)
+
+arima(temp_comp_random_ts, c(1,0,0))
+
+
+  #### Nachfolgende Tage unter 0
 
