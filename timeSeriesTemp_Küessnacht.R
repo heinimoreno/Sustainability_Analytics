bb6a07cfd1333d489578e3c86d6b52521bc2a27d
Milica Pajkic <milica.pajkic@stud.hslu.ch>
2024-02-09 11:18:20 +0100

    Küssnacht Forecast

--- timeSeriesTemp_Küessnacht.R
+++ timeSeriesTemp_Küessnacht.R
@@ -11,7 +11,7 @@ library(magrittr)
 #### Clean Files ####
 
 # Engelberg einlesen
-kuessnacht <- read.csv("Seebodenalp_data.csv",sep=';', na.strings=c('-',''))
+kuessnacht <- read.csv("Daten/Seebodenalp_data.csv",sep=';', na.strings=c('-',''))
 
 kuessnacht$time <- as.Date(as.character(kuessnacht$time), format = "%Y%m%d")
 kuessnacht_clean <- kuessnacht %>% select('time',
@@ -46,5 +46,44 @@ monthly_below_zero <- engelberg_filtered %>%
   mutate(year_month = paste(year, month, sep="-")) %>% # Create year-month column
   select(year_month, days_below_zero) # Select only the columns you want
 
-monthly_below_zero <- monthly_below_zero %>%
+monthly_below_zero_k <- monthly_below_zero %>%
   mutate(year_month = as.Date(paste(year_month, "01", sep="-")))
+
+# If you want to see the result
+glimpse(monthly_below_zero_k)
+range(monthly_below_zero_k$year_month)
+
+ts_month_k <- ts(data = monthly_below_zero_k$days_below_zero,
+               start = c(1990,01),
+               frequency = 4)
+autoplot(ts_month_k)
+
+# Decompose
+ts_month_k_dc <- decompose(ts_month_k)
+plot(ts_month_k_dc)
+
+# Stationary?
+adf.test(ts_month_k)
+## it is stationary
+
+acf(ts_month_k)
+acf(ts_month_k_dc$random,na.action=na.pass)
+pacf(ts_month_k_dc$random,na.action=na.pass)
+
+# Arima Model 
+month_auto_k <- auto.arima(ts_month_k, D=1, d=1)
+month_auto_k.1 <- auto.arima(ts_month_k)
+month_auto_k
+month_auto_k.1
+
+# Forecasting
+f <- forecast(month_auto_k, level=c(95), h=5*4)
+
+last_year <- as.numeric(format(time(ts_month_k)[length(ts_month_k)], "%Y"))
+
+
+# Plotting the forecast
+plot(f, main = "Forecast for the Next 5 Winter Months", 
+     xlab = "Time", ylab = "Forecast")
+
+
