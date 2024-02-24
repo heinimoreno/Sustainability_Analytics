36aef64a47983c4a11a3f45c6a1e82728948a561
morenoh18 <moreno.heini@hotmail.com>
2024-02-24 10:22:23 +0100

    Update EDA_Messreihe.r

--- EDA_Messreihe.r
+++ EDA_Messreihe.r
@@ -170,7 +170,8 @@ lines(trend_data$time, trend_data$fitted_values, col = 'red')
 
 
 #---------------------------------------------------------------------------------
-#----------------------------------------------------------------------------
+# Trendlinien Engelberg
+#---------------------------------------------------------------------------------
 # Laden der Bibliotheken
 library(tidyverse)
 library(lubridate)
@@ -258,3 +259,100 @@ intercept_zickzack <- coefficients_zickzack[1]  # Y-Achsenabschnitt
 print(paste("Steigung der Temperaturtrendlinie:", slope_trend))
 print(paste("Steigung der monatlichen Daten:", slope_monthly))
 print(paste("Steigung der zickzackförmigen Messdaten:", slope_zickzack))
+
+
+
+
+#---------------------------------------------------------------------------------
+# Trendlinien Küssnacht
+#---------------------------------------------------------------------------------
+#----------------------------------------------------------------------------
+#----------------------------------------------------------------------------
+# Laden der Bibliotheken
+library(tidyverse)
+library(lubridate)
+library(imputeTS)
+
+data_folder <- "Daten/"
+
+# Laden und Vorbereiten der Daten aus "Seebodenalp_data.csv"
+seebodenalp_data <- read.csv(paste0(data_folder, "Seebodenalp_data.csv"), sep=';', na.strings=c('-', ''))
+
+seebodenalp_data$time <- as.Date(as.character(seebodenalp_data$time), format = "%Y%m%d")
+seebodenalp_clean <- seebodenalp_data %>% 
+  select(time, tre200nn) %>% 
+  rename(temp = tre200nn) %>%
+  filter(year(time) > 1989)
+
+# Behandeln von NA-Werten und ungültigen Werten
+seebodenalp_clean$temp[is.infinite(seebodenalp_clean$temp) | is.nan(seebodenalp_clean$temp)] <- NA
+seebodenalp_clean <- na.omit(seebodenalp_clean)
+
+# Umwandeln des Datums in numerische Werte für die Regression
+seebodenalp_clean$year_numeric <- as.numeric(format(seebodenalp_clean$time, "%Y")) +
+                               (as.numeric(format(seebodenalp_clean$time, "%j")) - 1) / 365.25
+
+# Lineare Regression für die Temperaturtrendlinie
+seebodenalp_clean_trend <- lm(temp ~ year_numeric, data = seebodenalp_clean)
+
+# Vorhergesagte Werte basierend auf dem Modell
+predicted_values_seebodenalp <- predict(seebodenalp_clean_trend, newdata = data.frame(year_numeric = seebodenalp_clean$year_numeric))
+
+# Erstellen eines Dataframes zur Verwendung mit lines()
+trend_data_seebodenalp <- data.frame(time = seebodenalp_clean$time, fitted_values = predicted_values_seebodenalp)
+
+# Laden und Vorbereiten der Daten aus "Messreihe_Luzern.csv"
+luzern_messreihe <- read_csv(paste0(data_folder, "Messreihe_Luzern.csv"), 
+                                show_col_types = FALSE) %>%
+  mutate(Date = as.Date(paste(Year, Month, "01", sep = "-"), format = "%Y-%m-%d")) %>%
+  filter(Year >= 1990)
+
+# Lineare Regression für die monatlichen Daten
+lm_luzern_messreihe <- lm(Temperature ~ Year + Month, data = luzern_messreihe)
+
+# Vorhersagen für die Trendlinie
+luzern_messreihe$fitted_values <- predict(lm_luzern_messreihe, newdata = luzern_messreihe)
+
+# Lineare Regression für die zickzackförmigen Messdaten der Luzern-Messreihe
+lm_luzern_messreihe_zickzack <- lm(Temperature ~ Date, data = luzern_messreihe)
+
+# Vorhersagen für die Trendlinie der zickzackförmigen Daten
+luzern_messreihe$fitted_values_zickzack <- predict(lm_luzern_messreihe_zickzack, newdata = luzern_messreihe)
+
+# Plotten der Temperaturzeitreihen und Trendlinien
+ggplot() +
+  geom_line(data = seebodenalp_clean, aes(x = time, y = temp), lwd = 1) +
+  geom_line(data = trend_data_seebodenalp, aes(x = time, y = fitted_values), color = "blue", lwd = 2) +
+  geom_line(data = luzern_messreihe, aes(x = Date, y = fitted_values_zickzack), color = "red", lwd = 2) +
+  labs(x = "Date", y = "Temperature (°C)", title = "Temperature Time Series and Trend Lines for Seebodenalp & Luzern") +
+  theme_minimal()
+
+
+# Berechnen der Werte der linearen Regression für die Temperaturtrendlinie
+summary_seebodenalp_clean_trend <- summary(seebodenalp_clean_trend)
+print(summary_seebodenalp_clean_trend)
+
+# Berechnen der Werte der linearen Regression für die zickzackförmigen Messdaten der Seebodenalp-Messreihe
+summary_lm_seebodenalp_messreihe_zickzack <- summary(lm_seebodenalp_messreihe_zickzack)
+print(summary_lm_seebodenalp_messreihe_zickzack)
+
+
+# Koeffizienten für die Temperaturtrendlinie
+coefficients_trend_seebodenalp <- coef(seebodenalp_clean_trend)
+slope_trend_seebodenalp <- coefficients_trend_seebodenalp[2]  # Steigung
+intercept_trend_seebodenalp <- coefficients_trend_seebodenalp[1]  # Y-Achsenabschnitt
+
+# Koeffizienten für die monatlichen Daten
+coefficients_monthly_luzern <- coef(lm_luzern_messreihe)
+slope_monthly_luzern <- coefficients_monthly_luzern[2]  # Steigung
+intercept_monthly_luzern <- coefficients_monthly_luzern[1]  # Y-Achsenabschnitt
+
+# Koeffizienten für die zickzackförmigen Messdaten der Luzern-Messreihe
+coefficients_zickzack_luzern <- coef(lm_luzern_messreihe_zickzack)
+slope_zickzack_luzern <- coefficients_zickzack_luzern[2]  # Steigung
+intercept_zickzack_luzern <- coefficients_zickzack_luzern[1]  # Y-Achsenabschnitt
+
+# Ausgabe der Steigungen
+print(paste("Steigung der Temperaturtrendlinie (Seebodenalp):", slope_trend_seebodenalp))
+print(paste("Steigung der monatlichen Daten (Luzern):", slope_monthly_luzern))
+print(paste("Steigung der zickzackförmigen Messdaten (Luzern):", slope_zickzack_luzern))
